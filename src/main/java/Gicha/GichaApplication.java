5be075a3abeb5735dfb0c7aa4a43e2548e9d5aab
Lavrek <Lealucy@seznam.cz>
2023-01-11 22:12:38 +0100

    initial commit

---
+++ src/main/java/Gicha/GichaApplication.java
@@ -0,0 +1,13 @@
+package Gicha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GichaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GichaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Gicha/GichaApplicationTests.java
@@ -0,0 +1,13 @@
+package Gicha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GichaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
